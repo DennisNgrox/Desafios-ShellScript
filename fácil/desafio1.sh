15ae97514148fa8d8e60c1eca34af806ae3bb6e5
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-05-29 17:08:26 -0300

    Create desafio1.sh
    Criado Desafio-1 da pasta fácil

---
+++ fácil/desafio1.sh
@@ -0,0 +1,19 @@
+#  ____  _____ ____    _    _____ ___ ___            _
+# |  _ \| ____/ ___|  / \  |  ___|_ _/ _ \          / |
+# | | | |  _| \___ \ / _ \ | |_   | | | | |  _____  | |
+# | |_| | |___ ___) / ___ \|  _|  | | |_| | |_____| | |
+# |____/|_____|____/_/   \_\_|   |___\___/          |_|
+
+
+## Exclusão de arquivos:
+
+## Primeiro:
+## Rode os seguintes comandos para criar o laboratório e logo após resolva-o
+
+mkdir Desafio
+cd Desafio/
+touch {1..10}.txt
+touch {1..3}.exc
+
+# Desafio: Crie um loop para excluir todos os arquivos que terminam com .exc
+
