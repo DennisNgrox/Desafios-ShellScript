5aaf17541dbf1b58a15d614717ae69653258b062
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-06-03 20:48:53 -0300

    Update desafio3.sh
    Escriot desafio - 3

--- fácil/desafio3.sh
+++ fácil/desafio3.sh
@@ -6,4 +6,23 @@
 # | |_| | |___ ___) / ___ \|  _|  | | |_| | |_____|  ___) |
 # |____/|_____|____/_/   \_\_|   |___\___/          |____/
 
-echo "Em construção"
+
+
+## Primeiro:
+## Rode os seguintes comandos para criar o laboratório e logo após resolva-o
+
+#Criar pasta Desafio
+mkdir Desafio/Desafio3
+
+#Entrar na pasta Desafio
+cd Desafio/Desafio3
+
+# Crie o laboratório com os comandos abaixos:
+
+for i in {1..9}; do echo -e "ESCOLA=LINUX\nUBUNTU\nCLASSE=A\nSALA=18\nALUNO=OPENSOURCE\nDESAFIO=DESAFIO3\nAULA=1" > sub$i.txt; done
+
+# Desafio:
+
+# Substitua os seguintes campos de 10 arquivos de texto, utilizando loop:
+# Substitua "ALUNO=OPENSOURCE" por "ALUNO=DESAFIO3"
+# Substitua "AULA=1" POR "AULA=3"
