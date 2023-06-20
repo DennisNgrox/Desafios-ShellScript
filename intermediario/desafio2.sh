b97e4a10300491b158ea6841a78767a86d99f646
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-06-20 10:00:11 -0300

    Create desafio2.sh
    Criado desafio-2 intermediario

---
+++ intermediario/desafio2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo ' ____  _____ ____    _    _____ ___ ___       ____   '
+echo '|  _ \| ____/ ___|  / \  |  ___|_ _/ _ \     |___ \  '
+echo '| | | |  _| \___ \ / _ \ | |_   | | | | |_____ __) | '
+echo '| |_| | |___ ___) / ___ \|  _|  | | |_| |_____/ __/  '
+echo '|____/|_____|____/_/   \_\_|   |___\___/     |_____| '
+
+# Criar diretorio
+mkdir desafio/intermediario/
+
+# Acessar diretório
+cd desafio/intermediario/
+
+# Criar pasta desafio-2 e acessa-la
+mkdir desafio2 ; cd desafio2/
+
+# Criar arquivo
+touch archive
+
+# Aguardar alguns minutos...
+echo 'aguarde no minimo 3 minutos"
+
+sleep 180;
+
+echo 'Crie um script que valide a ultima vez que um arquivo foi alterado, esse valor precisa ser informado em minutos ou horas.'
+echo 'Exemplo: Arquivo alterado a 0h e 3 minutos.'
+
